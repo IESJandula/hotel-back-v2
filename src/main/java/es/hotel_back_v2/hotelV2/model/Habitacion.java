e738a76236aa153c1e6d9edfe73e02004b3ad2b3
Agencia <jararui1003@g.educaand.es>
2024-11-05 16:53:46 +0100

    005: Crear entidad Habitacion 008: Crear relacion Reserva-Habitacion

---
+++ src/main/java/es/hotel_back_v2/hotelV2/model/Habitacion.java
@@ -0,0 +1,77 @@
+package es.hotel_back_v2.hotelV2.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Habitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long numero;
+    private String tipo;
+    private double precio;
+    private String estado;
+    private int capacidad;
+
+    //Relacion con Reserva
+    @ManyToMany(mappedBy = "habitaciones")
+    private List<Reserva> reservas;
+
+    public Habitacion(String tipo, double precio, String estado, int capacidad) {
+        this.tipo = tipo;
+        this.precio = precio;
+        this.estado = estado;
+        this.capacidad = capacidad;
+    }
+    public Habitacion() {}
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" +
+                "numero=" + numero +
+                ", tipo='" + tipo + '\'' +
+                ", precio=" + precio +
+                ", estado='" + estado + '\'' +
+                ", capacidad=" + capacidad +
+                '}';
+    }
+}
