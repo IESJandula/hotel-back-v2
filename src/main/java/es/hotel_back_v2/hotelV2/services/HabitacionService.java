19a1c8fbf05fdf0d768a576f78407dfab07fd23b
Agencia <jararui1003@g.educaand.es>
2024-11-11 15:45:36 +0100

    016 Crear servicio de Habitacion: He hecho el metodo crud del servicio de habitacion.

--- src/main/java/es/hotel_back_v2/hotelV2/services/HabitacionService.java
+++ src/main/java/es/hotel_back_v2/hotelV2/services/HabitacionService.java
@@ -1,12 +1,15 @@
 package es.hotel_back_v2.hotelV2.services;
 
 import es.hotel_back_v2.hotelV2.model.Habitacion;
+import es.hotel_back_v2.hotelV2.model.Reserva;
 import es.hotel_back_v2.hotelV2.repositories.HabitacionRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HabitacionService {
@@ -15,7 +18,26 @@ public class HabitacionService {
     private HabitacionRepository habitacionRepository;
 
     @Transactional
-    public void crearHabitacion(Habitacion habitacion) { return habitacionRepository.save(habitacion);}
+    public void crearHabitacion(Habitacion habitacion) {
+        habitacionRepository.save(habitacion);
+    }
+
+    public void eliminarHabitacion(Long numero) {
+        habitacionRepository.deleteById(numero);
+    }
+
+    public List<Habitacion> buscarHabitaciones() {
+        return habitacionRepository.findAll();
+    }
+
+    public Optional<Habitacion> buscarHabitacionPorId(Long numero) {
+       return habitacionRepository.findById(numero);
+    }
+
+
+
+
+
 
 
 }
